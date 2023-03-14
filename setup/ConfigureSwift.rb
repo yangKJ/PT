23a1cccd06b5b492e24015e4ab2cb192cd181d4a
Condy <yangkj310@gmail.com>
2023-03-14 16:19:43 +0800

    Uncreate the file

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -22,7 +22,7 @@ module Pod
             # There has to be a single file in the Classes dir
             # or a framework won't be created
             
-            `touch Sources/Controller/ReplaceMe.swift`
+            #`touch Sources/View/ReplaceMe.swift` # 创建一个文件
             
             `mv ./templates/swift/* ./`
                                         
