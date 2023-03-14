85c71e73026a4bcaf8fa8a884585765bc4ad3fda
Condy <yangkj310@gmail.com>
2023-03-14 15:55:19 +0800

    Update rename

--- Sources/Util/Target.swift
+++ Sources/Util/Target.swift
@@ -1,17 +1,17 @@
 //
-//  PROJECTTarget.swift
-//  PROJECT
+//  ${POD_NAME}Target.swift
+//  ${POD_NAME}
 //
 //  Created by PROJECT_OWNER on TODAYS_DATE.
 //  See: https://github.com/yangKJ/Rickenbacker/blob/master/Rickenbacker/Modules/Mediator/Mediator/SecondTarget.swift
 
 import Foundation
 
-class PROJECTTarget: NSObject {
+class ${POD_NAME}Target: NSObject {
     /// 备注提示，这里必须加上`@objc`
     /// 否则会出现找不到该方法从而导致控制器为`nil`问题
     @objc func setupViewController() -> UIViewController? {
-        let vc = PROJECTViewController()
+        let vc = ${POD_NAME}ViewController()
         return vc
     }
 }
--- Sources/Util/Util.swift
+++ Sources/Util/Util.swift
@@ -1,6 +1,6 @@
 //
-//  PROJECTUtil.swift
-//  PROJECT
+//  ${POD_NAME}Util.swift
+//  ${POD_NAME}
 //
 //  Created by PROJECT_OWNER on TODAYS_DATE.
 //  https://github.com/yangKJ/Rickenbacker
@@ -8,18 +8,18 @@
 import Foundation
 import Rickenbacker
 
-struct PROJECTUtil {
-
-    internal static let moduleName = "PROJECT"
+struct ${POD_NAME}Util {
+    
+    internal static let moduleName = "${POD_NAME}"
 }
 
 extension Rickenbacker.R {
-
+    
     internal static func image(_ named: String) -> UIImage {
-        self.image(named, forResource: PROJECTUtil.moduleName)
+        self.image(named, forResource: ${POD_NAME}Util.moduleName)
     }
-
+    
     internal static func text(_ string: String) -> String {
-        self.text(string, forResource: PROJECTUtil.moduleName)
+        self.text(string, forResource: ${POD_NAME}Util.moduleName)
     }
 }
